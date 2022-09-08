963690bc91a6ae22918d42bfc3d753bfe97d04e9
Max <Maxhelbes@yandex.ru>
2022-09-08 21:06:45 +0300

    Restruction 2

--- Guitar/GuitarConvert.py
+++ Guitar/GuitarConvert.py
@@ -1,10 +1,10 @@
 from PIL import Image
-from Guitar.GuitarSheets import sheet
+from Resources.GuitarSheets import sheet
 
-imageResources = Image.open('GuitarResource.jpg')
-emptySpace = Image.open('EmptySpace.png')
+imageResources = Image.open('Guitar/Resources/GuitarResource.jpg')
+emptySpace = Image.open('Guitar/Resources/EmptySpace.png')
 
-inputText = open('music.txt')
+inputText = open('Guitar/music.txt')
 inputMusic = inputText.readlines()
 
 for i in range(len(inputMusic)):
@@ -15,13 +15,15 @@ print(inputMusic)
 cellWidht = 82
 cellHeight = 540
 
+SDVIG = 12
+
 for i in range(1, len(inputMusic)):
-    num = sheet[inputMusic[i]] - 0
+    num = sheet[inputMusic[i]] - 0 + SDVIG
     note = imageResources.crop((cellWidht * num, 0, cellWidht * (num+1), cellHeight))
     emptySpace.paste(note, (cellWidht * (i-1), 0))
 
 type = '.png'
-way = ''
+way = 'Guitar/Tabs/'
 name = way + inputMusic[0] + type
 print(name)
 emptySpace.save(name)
